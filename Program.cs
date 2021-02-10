e658603dd8c941bf1a3947964cfbf42ae73a8813
joserj2109 <78870364+joserj2109@users.noreply.github.com>
2021-02-10 13:21:27 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoSI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{// a:11 b:22 c:33
+			int a = 11;
+			int b = 22;
+			int c = 33;
+
+			Console.Write(a);
+			Console.Write(" ");
+			Console.Write(a);
+			Console.Write(" ");
+			Console.Write(c);
+			Console.Write(" ");
+			Console.WriteLine(b);
+
+			Console.WriteLine(" ");
+
+			Console.Write(b);
+			Console.Write(" ");
+			Console.Write(c );
+			Console.Write(" ");
+			Console.WriteLine(a );
+
+			Console.WriteLine(" ");
+
+			Console.Write(b);
+			Console.Write(" ");
+			Console.Write(c);
+			Console.Write(" ");
+			Console.Write(b);
+			Console.Write(" ");
+			Console.WriteLine(c);
+
+			Console.ReadLine();
+
+
+
+
+
+		}
+
+	}
+}
